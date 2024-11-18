23d13b2a9dd6244a143fb8011749daa18a56bc06
woghksedaa <20242119@sdhs.gwe.hs.kr>
2024-11-18 15:21:19 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import gdown
 
 # Google Drive 파일 ID
-file_id = '1NKIhMhUeRC0vPptHwT4it-LMYhamVDyi'
+file_id = '1H-7dIw1FpU6oq4cRdwdO2JU6j0WhBjgE'
 
 # Google Drive에서 파일 다운로드 함수
 @st.cache(allow_output_mutation=True)
